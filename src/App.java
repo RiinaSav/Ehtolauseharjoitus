df3b8392cfe1cb793b961b13d3a59a86deac3c7b
RiinaSav <savela.riina@gmail.com>
2024-10-06 17:06:08 +0300

    Tehtävä 1 tehty loppuun

--- src/App.java
+++ src/App.java
@@ -10,14 +10,44 @@
         int luku1 = 100;
         int luku2 = 10;
 
-        //  System.out.println("luku1 on yhtä suuri kuin luku2");
+        //  if (luku1 == luku2) {
+        //     System.out.println("luku1 on yhtä suuri kuin luku2");
+        //  }
+        //  else
+        //  {
+        //     System.out.println("luku1 ei ole yhtä suuri kuin luku2");
+        // }
 
-         if (luku1 == luku2) {
-            System.out.println("luku1 on yhtä suuri kuin luku2");
-         }
-         else
-         {
-            System.out.println("luku1 ei ole yhtä suuri kuin luku2");
-        }
+        // 1.2
+    //     if (luku1 > luku2) {
+    //         System.out.println("luku1 on suurempi kuin luku2");
+    //     }
+    //     else
+    //     {
+    //         System.out.println("luku1 ei ole suurempi kuin luku2");
+    // }
+
+    //   1.3
+
+//     if (luku1 >= luku2){
+//         System.out.println("luku1 on suurempi tai yhtä suuri kuin luku2");
+//     }
+//     else
+//     {
+//         System.out.println("luku1 on pienempi kuin luku2");
+//      }
+// }
+//       }
+
+    //   1.4
+
+    if (luku1 != luku2){
+        System.out.println("luku1 on eri suuri kuin luku2");
     }
-}
+    else
+    {
+        System.out.println("Luku1 on yhtä suuri kuin luku2");
+
+    }
+     }
+      }
