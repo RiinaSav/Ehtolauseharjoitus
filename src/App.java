3e646d04a753b3e4d6197493b0d54b7167b86800
RiinaSav <savela.riina@gmail.com>
2024-10-06 16:36:18 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,23 @@
+      // 1. Tutki onko luku1 yhtä suuri kuin luku2.
+      //   2. Tutki onko luku1 suurempi kuin luku2.
+      //   3. Tutki onko luku1 suurempi tai yhtä suuri kuin luku2.
+      //   4. Tutki onko luku1 eri suuri kuin luku2.
+      public class App {
+        public static void main(String[] args) throws Exception {
+       
+
+        // 1.1
+        int luku1 = 100;
+        int luku2 = 10;
+
+        //  System.out.println("luku1 on yhtä suuri kuin luku2");
+
+         if (luku1 == luku2) {
+            System.out.println("luku1 on yhtä suuri kuin luku2");
+         }
+         else
+         {
+            System.out.println("luku1 ei ole yhtä suuri kuin luku2");
+        }
+    }
+}
