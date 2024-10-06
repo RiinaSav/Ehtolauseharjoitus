538aba148050c84df19ec1cf5f60b32600cde5f2
RiinaSav <savela.riina@gmail.com>
2024-10-06 17:26:10 +0300

    Tehtävä 2 tehty

--- src/App.java
+++ src/App.java
@@ -7,8 +7,8 @@
        
 
         // 1.1
-        int luku1 = 100;
-        int luku2 = 10;
+        // int luku1 = 100;
+        // int luku2 = 10;
 
         //  if (luku1 == luku2) {
         //     System.out.println("luku1 on yhtä suuri kuin luku2");
@@ -41,13 +41,75 @@
 
     //   1.4
 
-    if (luku1 != luku2){
-        System.out.println("luku1 on eri suuri kuin luku2");
-    }
-    else
-    {
-        System.out.println("Luku1 on yhtä suuri kuin luku2");
+        // int luku1 = 10;
+        // int luku2 = 35;
+        // int luku3 = 26;
 
-    }
-     }
-      }
+    // if (luku1 != luku2){
+    //     System.out.println("luku1 on eri suuri kuin luku2");
+    // }
+    // else
+    // {
+    //     System.out.println("Luku1 on yhtä suuri kuin luku2");
+
+    // }
+    //  }
+    //   }
+
+
+// 2.1 Tutki onko luku1 ja luku2 yhtä suuria TAI luku2 ja luku3 yhtä suuria.
+
+// if (luku1 == luku2 || luku2 == luku3) {
+// System.out.println("Luku1 on yhtä suuri kuin luku2 tai luku2 on yhtä suuri kuin luku3");
+// }
+
+// 2.2 Tutki onko luku1 suurempi kuin luku2 JA luku1 yhtä suuri kuin luku3.
+
+    // if (luku1 > luku2 && luku1 == luku3) {
+    //     System.out.println("Luku1 on suurempi kuin luku2 ja luku1 on yhtä suuri kuin luku3");
+    // }
+
+// 2.3 Tutki onko luku1 ja luku2 ja luku3 yhtä suuria.
+
+// if (luku1 == luku2 && luku2 == luku3); {
+
+// System.out.println("kaikki samoja");
+
+// }
+// }
+// }
+
+// 2.4 Tutki onko luku1 suurempi kuin luku2. Jos ei ole, tutki onko luku2 suurempi kuin luku3.
+
+// if (luku1 > luku2) {
+//     System.out.println("on");
+// }   
+// else { 
+//     System.out.println("ddd");
+// }
+// }
+// }
+
+// if (luku1 > luku2) {
+//     System.out.println("on");
+// }   
+// else if (luku2 > luku3) { 
+//     System.out.println("luku 2 > luku3");
+// }
+// }
+// }
+
+    // int luku1 = 26;
+    // int luku2 = 10;
+    // int luku3 = 26;
+    
+//2.5 Tutki onko luku1 ja luku2 yhtä suuria. Jos ei ole, tutki, onko luku1 ja luku 3 yhtä suuria.
+
+// if (luku1 == luku2) {
+//     System.out.println("jees"); 
+// }
+//     else if (luku1 == luku3) {
+//         System.out.println("onhan ne");
+//  }
+//     }
+// }
